534dd0c265d5a0d197509bbf86783638924cfd88
ssooouu <ssooouu@yandex.ru>
2024-09-03 23:31:17 +0500

    Create module_2_2.py

---
+++ module_2_2.py
@@ -0,0 +1,22 @@
+#1
+first = 123
+second = 456
+third = 789
+
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+#2
+first = 42
+second = 69
+third = 42
+
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
