d40a5cd5ab23ca7181db3e6e82b1d77905ba79e8
ssooouu <ssooouu@yandex.ru>
2024-09-05 16:19:26 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,6 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
 while len(my_list) > a and my_list[a] >= 0:
-    if my_list[a] >= 0:
+    if my_list[a] > 0:
         (print(my_list[a]))
     a += 1
-
