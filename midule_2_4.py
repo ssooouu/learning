03ec9e7cecf99da89484026b18dea1b3529f880a
ssooouu <ssooouu@yandex.ru>
2024-09-07 02:18:26 +0500

    Create midule_2_4.py

---
+++ midule_2_4.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    for j in numbers[1: ]:
+        if i % j == 0 and i / j != 1:
+            not_primes.append(i)
+            break
+        if i % j == 0 and i / j == 1:
+            primes.append(i)
+            break
+        if i / j <= 1:
+            break
+print(primes)
+print(not_primes)
