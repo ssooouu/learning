a6333299d038a05b082ab40b50b3beabf88e1ea7
ssooouu <ssooouu@yandex.ru>
2024-09-11 15:25:18 +0500

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -4,24 +4,21 @@ def count_calls():
     calls += 1
 
 def string_info(string):
-    global calls
-    calls += 1
-    print(len(string), string.lower(), string.upper())
+    count_calls()
+    len(string), string.lower(), string.upper()
+    return (len(string), string.lower(), string.upper())
 
 def is_contains(string, list_to_search):
     n = 0
-    w = 0
-    global calls
-    calls += 1
+    count_calls()
     while n < len(list_to_search):
         if string.lower() == list_to_search[n].lower():
-            print(True)
-        if string.lower() != list_to_search[n].lower():
+            return (True)
+        elif string.lower() != list_to_search[n].lower():
             n += 1
             continue
-        print(False)
-        break
-# У меня в ответе почему то получаются какие то None. Откуда они берутся и что это вообще такое?
+    return (False)
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
