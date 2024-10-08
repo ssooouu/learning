056a25cb1a5c0c85603821585830591737e340a6
ssooouu <ssooouu@yandex.ru>
2024-10-08 19:15:27 +0500

    Create module_5_4.py

---
+++ module_5_4.py
@@ -0,0 +1,43 @@
+class Example:
+    def __new__(cls, *args, **kwargs):
+        print(args)
+        print(kwargs)
+        return object.__new__(cls)
+
+    def __init__(self, first, second, third):
+        print(first)
+        print(second)
+        print(third)
+
+
+ex = Example('data', second=25, third=3.14)
+
+
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        cls.houses_history.append(args)
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
