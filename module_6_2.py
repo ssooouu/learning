3b9c564e5800b9dee5310e0d135c6b05ebe186b1
ssooouu <ssooouu@yandex.ru>
2024-10-26 19:27:16 +0500

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -1,19 +1,14 @@
 class Vehicle:
-    def __init__(self, owner, __model, __color, __engine_power):
+    def __init__(self, owner, __model, __color, __engine_power, ):
         self.owner = owner
         self.model = __model
         self.engine_power = __engine_power
         self.color = __color
-        self.__COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def set_color(self, new_color):
-        if new_color.lower() in self.__COLOR_VARIANTS:
-            self.color = new_color
-        else:
-            print(f'Нельзя сменить цвет машины на {new_color}')
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
     def get_model(self):
         return self.model
@@ -26,9 +21,13 @@ class Sedan(Vehicle):
 
     def print_info(self):
         print(f'Модель: {Sedan.get_model(self)}, Мощность двигателя: {Sedan.get_horsepower(self)}, '
-                f'Цвет: {Sedan.get_color(self)}, Владелец: {self.owner}')
-
-
+              f'Цвет: {Sedan.get_color(self)}, Владелец: {self.owner}')
+        
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.color = new_color
+        else:
+            print(f'Нельзя сменить цвет машины на {new_color}')
 
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
@@ -44,3 +43,4 @@ vehicle1.owner = 'Vasyok'
 
 # Проверяем что поменялось
 vehicle1.print_info()
+
