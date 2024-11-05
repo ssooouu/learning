bc25ae132a5d346ca0c975422d33a4ae3ae3e401
ssooouu <ssooouu@yandex.ru>
2024-11-05 22:23:05 +0500

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 class Product:
     def __init__(self, name, weight, category):
         self.name = name
@@ -8,30 +5,34 @@ class Product:
         self.category = category
 
     def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}'
+        a = (self.name, self.weight, self.category)
+
+        return str(a)
 
 
 class Shop:
-    def __init__(self, *products):
-        self.prod = products
+    def __init__(self):
         self.__file_name = 'products.txt'
+        self.prod_list = 0
+        self.file = 0
 
     def get_products(self):
         self.file = open(self.__file_name, 'r')
-        pprint(self.file.read())
+        self.prod_list = self.file.read()
         self.file.close()
+        return self.prod_list
 
     def add(self, *products):
-        self.file = open(self.__file_name, 'r')
-        pprint(self.file.read())
-        if self.file[0] in products:
-            print('Такой товар уже есть')
-        else:
-            for num in products:
+        list_pord = self.get_products()
+        for prod in products:
+            if list_pord.find(prod.name) != -1:
                 self.file = open(self.__file_name, 'a')
-                self.file.write(str(f'{num} \n'))
+                self.file.write(str(f'This produkt\n'))
                 self.file.close()
-        self.file.close()
+            else:
+                self.file = open(self.__file_name, 'a')
+                self.file.write(str(f'{prod} \n'))
+                self.file.close()
 
 
 s1 = Shop()
@@ -40,7 +41,7 @@ p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
 print(p2)  # __str__
-
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
+
