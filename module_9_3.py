967124d58e7d502c2eff10a45ba3c301489fb351
ssooouu <ssooouu@yandex.ru>
2024-12-27 19:14:15 +0500

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,10 +1,9 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_strings = (len(x[0]) - len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
-second_strings = (len(x) == len(y) for x in first for y in second)
-# Я не запутался. Правильно ли я сделал первую строку?
-# И мне нужна помощь со второй строкой
+first_strings = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+second_strings = (len(second[c]) == len(first[c]) for c in range(len(second)))
 
 print(list(first_strings))
 print(list(second_strings))
+
