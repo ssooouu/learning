699b5b78def995649961b8735ae7f5cd9c53a9c4
Алексей <ssooouu@yandex.ru>
2025-01-13 19:59:30 +0500

    Create tests_12_3.py

---
+++ tests_12_3.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest import TestCase
+from runner import Runner
+
+
+class RunnerTest(TestCase):
+    def test_wolk(self):
+        human_test = Runner("Алина")
+        n = 0
+        while n != 10:
+            human_test.walk()
+            n += 1
+        self.assertEqual(human_test.distance, 50)
+
+    @unittest.skipIf(True, 'Не люблю бегать')
+    def test_run(self):
+        human_test = Runner("Леха")
+        n = 0
+        while n != 10:
+            human_test.run()
+            n += 1
+        self.assertEqual(human_test.distance, 100)
+
+    @unittest.skipIf(True, 'заморожен')
+    def test_challenge(self):
+        human_test_2 = Runner("Леха")
+        n = 0
+        while n != 10:
+            human_test_2.run()
+            n += 1
+        self.assertEqual(human_test_2.distance, 100)
+
+        human_test_1 = Runner("Алина")
+        n = 0
+        while n != 10:
+            human_test_1.walk()
+            n += 1
+        self.assertEqual(human_test_1.distance, 50)
+
+        self.assertNotEqual(human_test_2.distance, human_test_1)
+
+if __name__ == '__main__':
+    unittest.main()
